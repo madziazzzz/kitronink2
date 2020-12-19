e7333ed5d896622e049fe43aecc5ee569c265beb
madziazzzz <76257178+madziazzzz@users.noreply.github.com>
2020-12-19 17:58:52 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let swiatlo = 0
+basic.forever(function () {
+    swiatlo = pins.analogReadPin(AnalogPin.P0)
+    if (swiatlo > 20) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.showLeds(`
+            # # # . .
+            . # # # .
+            . . # # .
+            . # # # .
+            # # # . .
+            `)
+    }
+})
